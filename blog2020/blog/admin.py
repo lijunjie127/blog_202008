e650a658e53771773af2b494b9a77794562176cd
lijunjie <18811508970@163.com>
2020-08-06 06:09:40 +0800

    只查看本人发表的文章

--- blog2020/blog/admin.py
+++ blog2020/blog/admin.py
@@ -7,7 +7,7 @@ from .models import Post, Category, Tag
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
+    list_display = ('name', 'status', 'is_nav', 'owner', 'created_time', 'post_count')
     fields = ('name', 'status', 'is_nav')
 
     def save_model(self, request, obj, form, change):  # 后台只能编辑自己的文章
@@ -28,11 +28,30 @@ class TagAdmin(admin.ModelAdmin):
         obj.owner = request.user
         return super(TagAdmin, self).save_model(request, obj, form, change)
 
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """自定义过滤器只展示当前用户分类"""
+
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=category_id)
+        return queryset
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'status', 'created_time', 'operator']
+    exclude = ('owner', )
+    list_display = ['title', 'category', 'status', 'owner', 'created_time', 'operator']
     list_display_links = []
     list_filter = ['category', ]
+    # list_filter = [CategoryOwnerFilter]  # 使用过滤器
     search_fields = ['title', 'category__name']
     actions_on_top = False
     actions_on_bottom = True
@@ -41,8 +60,7 @@ class PostAdmin(admin.ModelAdmin):
     save_on_top = True
     fields = (
         ('category', 'title'),
-        'desc',
-        'status',
+        ('desc', 'status'),
         'content',
         'tag',
     )
@@ -58,3 +76,11 @@ class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
+
+    class Media:
+        css = {'all': 'https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css'}
+        js = {'https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js'}
