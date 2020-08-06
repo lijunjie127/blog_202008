b42be345ff925a41e6cd8010986af15fe4081baf
lijunjie <18811508970@163.com>
2020-08-07 06:00:16 +0800

    在admin页面上查看操作日志

--- blog2020/blog/admin.py
+++ blog2020/blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
 from django.urls import reverse
 from django.utils.html import format_html
 
@@ -96,3 +97,6 @@ class PostAdmin(admin.ModelAdmin):
         js = {'https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js'}
 
 
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['object_repr', 'object_id', 'action_flag', 'user', 'change_message']
